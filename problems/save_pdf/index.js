99786e5f697d15cc41d3258a3dfcb6b8b77798db
Max Ogden <max@maxogden.com>
2016-02-23 22:27:54 -0800

    finish save pdf

--- problems/save_pdf/index.js
+++ problems/save_pdf/index.js
@@ -6,7 +6,7 @@ var mdrender = require('../../markdown-render.js')
 module.exports = function () {
   var problem = {}
   problem.problem = mdrender(fs.readFileSync(path.join(__dirname, 'problem.md')).toString()) + '\n'
-  problem.solution = '\nRun "elementary-electron" again to select the next challenge.\n'
+  problem.solution = 'That was the last challenge!\nFor more Electron ideas check out https://github.com/sindresorhus/awesome-electron\nFor more workshops like this check out http://nodeschool.io\n'
 
   problem.verify = function (args, cb) {
     try {
@@ -18,7 +18,7 @@ module.exports = function () {
     cb(true)
   }
 
-  problem.pass = '\n' + chalk.green('SUCCESS!') + ' Run elementary-electron again to select the next challenge'
+  problem.pass = '\n' + chalk.green('SUCCESS!') + ' You did it!'
   problem.fail = '\n' + chalk.red('FAIL!') + ' Your app has some issues.'
   problem.run = function (args) {
     console.error('Run is not necessary in this challenge')
