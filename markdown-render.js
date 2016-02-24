e65dbebddf657d3ca4b96c867e0585e76608c0e2
freeman <the.freeman.lab@gmail.com>
2016-02-23 19:23:32 -0800

    custom bold formatting for code

--- markdown-render.js
+++ markdown-render.js
@@ -1,6 +1,12 @@
 var ansimd = require('ansimd')
 
 // monkeypatch
-ansimd.renderer.code = ansimd.renderer.paragraph
+ansimd.renderer.code = function (text) {
+  return '\n' + bold(text) + '\n'
+}
+
+function bold (str) {
+  return '\033[1m' + str + '\033[0m'
+}
 
 module.exports = ansimd
