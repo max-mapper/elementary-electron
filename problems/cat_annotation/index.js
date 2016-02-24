8173b7e70fd255fc88c43567c4bf0d81f53b8151
freeman <the.freeman.lab@gmail.com>
2016-02-23 21:21:13 -0800

    replace todo

--- problems/cat_annotation/index.js
+++ problems/cat_annotation/index.js
@@ -6,7 +6,7 @@ var mdrender = require('../../markdown-render.js')
 module.exports = function () {
   var problem = {}
   problem.problem = mdrender(fs.readFileSync(path.join(__dirname, 'problem.md')).toString()) + '\n'
-  problem.solution = 'TODO'
+  problem.solution = 'Run "elementary-electron" again to select the next challenge.\n'
 
   problem.verify = function (args, cb) {
     try {
